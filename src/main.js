ff398e7fba8070d78a46674e116e913ce5361985
Akash Goswami <akashgoswami70870@gmail.com>
2023-10-15 09:54:48 +0530

    Added the noti using nodemailer

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -10,7 +11,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-    });
+        sendBasicEmail(
+            'support@admin.com',
+            'moviebookingappservice@gmail.com',
+            'This is a testing email',
+            'Hey, how are you, I hope you like the support'
+        );
+    }); 
 }
 
 setupAndStartServer();
